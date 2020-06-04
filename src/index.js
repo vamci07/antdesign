076c6f16e815b333f8b37054474b5f212ecc0c57
Vamshi Maddur <vamci07@gmail.com>
2020-06-04 00:17:03 -0400

    Default theme

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'antd/dist/antd.css';
-import './index.css';
 import App from './App';
 
 import 'utils/i18n';
