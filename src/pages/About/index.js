0057b64d2ddc6aed2e2c5c446bc378405dd6cd51
Vamshi Maddur <vamci07@gmail.com>
2020-06-04 00:26:47 -0400

    Customize pages

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function About() {
-  return <div>Hello, I'm in about page.</div>;
+  return (
+    <div className="site-layout-background" style={{ margin: 24, padding: 24, minHeight: 360 }}>
+      Hello, I'm in about page.
+    </div>
+  );
 }
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
+    <div className="site-layout-background" style={{ margin: 24, padding: 24, minHeight: 360 }}>
       <span>Yes, please contact me!</span>
     </div>
   );
